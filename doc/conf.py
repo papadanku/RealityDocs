581f2b02e900b1d3fbf781dee1670cc658d75954
papadanku <115061077+papadanku@users.noreply.github.com>
2024-08-03 12:31:49 -0700

    Cleanups

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'RealityDocs'
+project = 'The Project Reality Documentation'
 copyright = '2024, The Project Reality Team'
 author = 'The Project Reality Team'
 
@@ -16,7 +16,9 @@ author = 'The Project Reality Team'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
